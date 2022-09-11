968a83c864a6f2e9c7214f98deb5e618a24fb17a
Przemysław Dominikowski <przemyslaw.dominikowski@intel.com>
2022-09-11 15:29:29 +0200

    Unique endpoint paths
    Update endpoint paths to have unique prefix. This is a known limitation of Gin framework.

--- controllers/controller.go
+++ controllers/controller.go
@@ -32,20 +32,20 @@ func ServeBackend() {
 	authorized.Use(middleware.Authenticate())
 	{
 		// volume
-		authorized.POST("/volume", CreateVolume)
-		authorized.PUT("/volume/:VolumeUUID", UpdateVolume)
-		authorized.DELETE("/volume/:VolumeUUID", DeleteVolume)
-		authorized.GET("/volume/:VolumeUUID", GetVolume)
+		authorized.POST("/volume/manage", CreateVolume)
+		authorized.PUT("/volume/manage/:VolumeUUID", UpdateVolume)
+		authorized.DELETE("/volume/manage/:VolumeUUID", DeleteVolume)
+		authorized.GET("/volume/manage/:VolumeUUID", GetVolume)
 
 		authorized.POST("/volume/share/:VolumeUUID", ShareVolume)
 
 		authorized.GET("/volume/volumes", GetVolumes)
 
 		// disk
-		authorized.POST("/disk", DiskCreate)
-		authorized.PUT("/disk/:DiskUUID", DiskUpdate)
-		authorized.DELETE("/disk/:DiskUUID", DiskDelete)
-		authorized.GET("/disk/:DiskUUID", DiskGet)
+		authorized.POST("/disk/manage", DiskCreate)
+		authorized.PUT("/disk/manage/:DiskUUID", DiskUpdate)
+		authorized.DELETE("/disk/manage/:DiskUUID", DiskDelete)
+		authorized.GET("/disk/manage/:DiskUUID", DiskGet)
 		authorized.POST("/disk/oauth", DiskOAuth)
 
 		authorized.GET("/disk/disks", GetDisks)
@@ -54,12 +54,12 @@ func ServeBackend() {
 		authorized.DELETE("/disk/associate/:DiskUUID", DiskDissociate)
 
 		// file
-		authorized.POST("/file/:BlockUUID", FileUpload)
-		authorized.GET("/file/download/:BlockUUID", FileDownload)
+		authorized.POST("/file/io/:BlockUUID", FileUpload)
+		authorized.GET("/file/io/:BlockUUID", FileDownload)
 
-		authorized.PUT("/file/:FileUUID", FileRename)
-		authorized.DELETE("/file/:FileUUID", FileRemove)
-		//authorized.GET("/file/:FileUUID", FileGet)
+		authorized.PUT("/file/manage/:FileUUID", FileRename)
+		authorized.DELETE("/file/manage/:FileUUID", FileRemove)
+		authorized.GET("/file/manage/:FileUUID", FileGet)
 
 		authorized.GET("/file/request", FileRequest)
 		authorized.POST("/file/request/complete/:FileUUID", FileRequestComplete)
