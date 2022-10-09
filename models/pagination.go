46043566cb6440b08d903a2ff5d4ee6469c365e7
Brotholomew <bartoszek.blach@gmail.com>
2022-10-09 22:25:37 +0200

    [DCFS-71] added pagination
    Signed-off-by: Brotholomew <bartoszek.blach@gmail.com>

---
+++ models/pagination.go
@@ -0,0 +1,35 @@
+package models
+
+import "math"
+
+type Pagination struct {
+	CurrentPage   int `json:"currentPage"`
+	TotalPages    int `json:"totalPages"`
+	PerPage       int `json:"perPage"`
+	RecordsOnPage int `json:"recordsOnPage"`
+	TotalRecords  int `json:"totalRecords"`
+}
+
+type PaginationData struct {
+	Pagination Pagination
+	Data       interface{}
+}
+
+func Paginate(collection []interface{}, page int, _perPage int) *PaginationData {
+	var paginationData *PaginationData = new(PaginationData)
+	var totalPages int = int(math.Ceil(float64(len(collection)) / float64(_perPage)))
+	var recordsOnPage int = int(math.Min(float64(_perPage), float64(len(collection)*((page-1)*_perPage))))
+	var data []interface{} = nil
+
+	for i := len(collection) * ((page - 1) * _perPage); i < len(collection)*((page-1)*_perPage)+recordsOnPage; i++ {
+		data = append(data, collection[i])
+	}
+
+	paginationData.Data = data
+	paginationData.Pagination.CurrentPage = page
+	paginationData.Pagination.TotalPages = totalPages
+	paginationData.Pagination.PerPage = _perPage
+	paginationData.Pagination.RecordsOnPage = recordsOnPage
+	paginationData.Pagination.TotalRecords = len(collection)
+	return nil
+}
