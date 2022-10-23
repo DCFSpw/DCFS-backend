48b86a06db79bf2ed24202c26db70c4ecd8ad799
Brotholomew <bartoszek.blach@gmail.com>
2022-10-23 22:05:46 +0200

    [DCFS-57] update tests
    Signed-off-by: Brotholomew <bartoszek.blach@gmail.com>

--- models/ut_partitioner_test.go
+++ models/ut_partitioner_test.go
@@ -1,17 +1,17 @@
 package models
 
 import (
-	"dcfs/models/disk/GDriveDisk"
 	"github.com/google/uuid"
 	. "github.com/smartystreets/goconvey/convey"
 	"testing"
 )
 
 func TestDummyPartitioner(t *testing.T) {
-	var volume Volume = Volume{}
+	var volume Volume = Volume{UUID: uuid.New()}
 	var partitioner *DummyPartitioner = NewDummyPartitioner(&volume)
-	var firstDisk *GDriveDisk.GDriveDisk = GDriveDisk.NewGDriveDisk()
-	var secondDisk *GDriveDisk.GDriveDisk = GDriveDisk.NewGDriveDisk()
+
+	var firstDisk Disk = &dummyDisk{}
+	var secondDisk Disk = &dummyDisk{}
 
 	firstDisk.SetUUID(uuid.New())
 	secondDisk.SetUUID(uuid.New())
