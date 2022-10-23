89003940b66065a315addded237742dc1f1db1b1
Brotholomew <bartoszek.blach@gmail.com>
2022-10-23 22:05:46 +0200

    [DCFS-57] update tests
    Signed-off-by: Brotholomew <bartoszek.blach@gmail.com>

---
+++ models/ut_mock_disk.go
@@ -0,0 +1,67 @@
+package models
+
+import (
+	"dcfs/apicalls"
+	"dcfs/db/dbo"
+	"dcfs/models/credentials"
+	"github.com/google/uuid"
+)
+
+type dummyDisk struct {
+	UUID   uuid.UUID
+	Volume *Volume
+}
+
+/* Mandatory Disk interface implementations */
+
+func (d *dummyDisk) Upload(blockMetadata *apicalls.BlockMetadata) error {
+	panic("Unimplemented")
+}
+
+func (d *dummyDisk) Download(blockMetadata *apicalls.BlockMetadata) error {
+	panic("Unimplemented")
+}
+
+func (d *dummyDisk) Rename(blockMetadata *apicalls.BlockMetadata) error {
+	panic("Unimplemented")
+}
+
+func (d *dummyDisk) Remove(blockMetadata *apicalls.BlockMetadata) error {
+	panic("Unimplemented")
+}
+
+func (d *dummyDisk) SetVolume(volume *Volume) {
+	d.Volume = volume
+}
+
+func (d *dummyDisk) GetVolume() *Volume {
+	return d.Volume
+}
+
+func (d *dummyDisk) SetUUID(uuid uuid.UUID) {
+	d.UUID = uuid
+}
+
+func (d *dummyDisk) GetUUID() uuid.UUID {
+	return d.UUID
+}
+
+func (d *dummyDisk) GetCredentials() credentials.Credentials {
+	panic("Unimplemented")
+}
+
+func (d *dummyDisk) SetCredentials(credentials credentials.Credentials) {
+	panic("Unimplemented")
+}
+
+func (d *dummyDisk) CreateCredentials(c string) {
+	panic("Unimplemented")
+}
+
+func (d *dummyDisk) GetProviderUUID() uuid.UUID {
+	panic("Unimplemented")
+}
+
+func (d *dummyDisk) GetDiskDBO(userUUID uuid.UUID, providerUUID uuid.UUID, volumeUUID uuid.UUID) dbo.Disk {
+	panic("Unimplemented")
+}
