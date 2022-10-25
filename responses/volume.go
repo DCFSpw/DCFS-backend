d758dae649d14d98fc03bf919d9c8ad2f68ecd46
Przemysław Dominikowski <przemyslaw.dominikowski@intel.com>
2022-10-25 21:17:06 +0200

    Use common response

--- responses/volume.go
+++ responses/volume.go
@@ -2,13 +2,8 @@ package responses
 
 import "dcfs/db/dbo"
 
-type VolumeDataSuccessResponse struct {
-	Success bool       `json:"success"`
-	Data    dbo.Volume `json:"data"`
-}
-
-func NewVolumeDataSuccessResponse(volumeData *dbo.Volume) *VolumeDataSuccessResponse {
-	var r *VolumeDataSuccessResponse = new(VolumeDataSuccessResponse)
+func NewVolumeDataSuccessResponse(volumeData *dbo.Volume) *EmptySuccessResponse {
+	var r *EmptySuccessResponse = new(EmptySuccessResponse)
 	r.Success = true
 	r.Data = *volumeData
 	return r
