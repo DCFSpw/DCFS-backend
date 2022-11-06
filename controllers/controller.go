8d57da4adb4fddc452ab02c3d488ada9ed1bb4bd
Przemysław Dominikowski <przemyslaw.dominikowski@intel.com>
2022-11-06 18:43:37 +0100

    [DCFS-100] Update file endpoint

--- controllers/controller.go
+++ controllers/controller.go
@@ -72,21 +72,17 @@ func ServeBackend() {
 		authorized.GET("/files/manage", GetFiles)
 
 		authorized.POST("/files/upload", InitFileUploadRequest)
+		//authorized.POST("/files/upload/:FileUUID", InitFileDownloadRequest)
 		authorized.POST("/files/upload/:FileUUID", CompleteFileUploadRequest)
 
-		//authorized.GET("/files/upload", InitFileDownloadRequest)
-
 		authorized.POST("/files/block/:BlockUUID", UploadBlock)
-		//authorized.GET("/file/io/:FileUUID", FileDownload)
+		//authorized.GET("/files/block/:BlockUUID", DownloadBlock)
 
 		authorized.POST("/file/request", FileRequest)
 		authorized.POST("/file/request/complete/:FileUUID", FileRequestComplete)
 
-		//authorized.PUT("/file/manage/:FileUUID", FileRename)
-		//authorized.DELETE("/file/manage/:FileUUID", FileRemove)
-
-		//authorized.POST("/file/share/:FileUUID", FileShare)
-		//authorized.DELETE("/file/share/FileUUID", FileShareRemove)
+		authorized.PUT("/files/manage/:FileUUID", UpdateFile)
+		//authorized.DELETE("/files/manage/:FileUUID", FileRemove)
 
 		// Providers
 		authorized.GET("/providers", GetProviders)
