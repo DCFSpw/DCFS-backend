d700f216f20b5db9fc6141b3b8460b904964f61e
Przemysław Dominikowski <przemyslaw.dominikowski@intel.com>
2022-11-12 18:45:16 +0100

    [DCFS-146] Set Throughput partitioner ID to proper number

--- requests/volume.go
+++ requests/volume.go
@@ -3,7 +3,7 @@ package requests
 type VolumeSettingsRequest struct {
 	Backup        int `json:"backup" binding:"required,min=1,max=2"`
 	Encryption    int `json:"encryption" binding:"required,min=1,max=2"`
-	FilePartition int `json:"filePartition" binding:"required,min=1,max=2"`
+	FilePartition int `json:"filePartition" binding:"required,min=1,max=3"`
 }
 
 type VolumeCreateRequest struct {
