f29c533c2ae1e16990c78d6eea8ff44c7340178b
Przemysław Dominikowski <przemyslaw.dominikowski@intel.com>
2022-11-15 16:11:27 +0100

    [DCFS-92] Add comments to validators

--- validators/user.go
+++ validators/user.go
@@ -5,6 +5,14 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+// ValidateUserPassword - validate provided password against the hashed password
+//
+// params:
+//   - hashedPassword: string with user's password hashed by backend
+//   - providedPassword: string password to verify
+//
+// return type:
+//   - errorCode: (constant.SUCCESS if password match, constant.AUTH_INVALID_PASSWORD otherwise)
 func ValidateUserPassword(hashedPassword string, providedPassword string) string {
 	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(providedPassword))
 	if err != nil {
