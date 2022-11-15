91dda3a9df91f2078061a4e78db100e09dcbfd4e
Przemysław Dominikowski <przemyslaw.dominikowski@intel.com>
2022-11-15 20:33:57 +0100

    [DCFS-92] Add comments to block

--- models/block.go
+++ models/block.go
@@ -18,6 +18,23 @@ type Block struct {
 	Order  int
 }
 
+// NewBlock - create new block model based on provided data
+//
+// This function creates block model used internally by backend based on
+// block data obtained from database.
+//
+// params:
+//   - _UUID uuid.UUID: block UUID
+//   - _userUUID uuid.UUID: UUID of the user who owns the block
+//   - _file File: UUID of the file to which the block belongs
+//   - _disk Disk: UUID of the disk on which the block is stored
+//   - _size int: size of the block in bytes
+//   - _checksum string: checksum of the block
+//   - _status int: transport status of the block
+//   - _order int: order of the block in the file
+//
+// return type:
+//   - *models.Block: created block model
 func NewBlock(_UUID uuid.UUID, _userUUID uuid.UUID, _file File, _disk Disk, _size int, _checksum string, _status int, _order int) *Block {
 	var block *Block = new(Block)
 
@@ -33,6 +50,16 @@ func NewBlock(_UUID uuid.UUID, _userUUID uuid.UUID, _file File, _disk Disk, _siz
 	return block
 }
 
+// NewBlockFromDBO - create new block model based on block DBO
+//
+// This function creates block model used internally by backend based on
+// block data obtained from database.
+//
+// params:
+//   - _block *dbo.Block: block DBO data (from database)
+//
+// return type:
+//   - *models.Block: created block model
 func NewBlockFromDBO(_block *dbo.Block) *Block {
 	return &Block{
 		UUID:     _block.UUID,
