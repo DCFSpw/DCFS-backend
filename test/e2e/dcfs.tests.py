ca6d862c0023fd13d14f76bd71e57bda2d2df658
Brotholomew <bartoszek.blach@gmail.com>
2022-12-04 17:14:00 +0100

    [DCFS-103][DCFS-145] e2e test: user
    Signed-off-by: Brotholomew <bartoszek.blach@gmail.com>

--- test/e2e/dcfs.tests.py
+++ test/e2e/dcfs.tests.py
@@ -1,13 +1,16 @@
 import selenium
 import unittest
 
+import utils
 from config import Config
 from utils import Logger
 
 from volume_tests import VolumeTests
+from user_test import UserTests
 
 Config.set_up()
 
 if __name__ == '__main__':
-    Logger.debug_level = 1
-    unittest.main(argv=Config.parse_test_args())
+    Logger.debug_level = 0
+    print(utils.Bcolors.ENDC)
+    unittest.main(argv=Config.parse_test_args(), warnings='ignore')
