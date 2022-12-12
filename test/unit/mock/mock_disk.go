fff9561bebd814f9d388e0c63f0c4c6e8b41a15e
Brotholomew <bartoszek.blach@gmail.com>
2022-12-12 23:22:26 +0100

    [DCFS-156] fix UT
    Signed-off-by: Brotholomew <bartoszek.blach@gmail.com>

--- test/unit/mock/mock_disk.go
+++ test/unit/mock/mock_disk.go
@@ -7,6 +7,7 @@ import (
 	"dcfs/models"
 	"dcfs/models/credentials"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"os"
 	"time"
@@ -175,11 +176,11 @@ func (d *MockDisk) AssignDisk(disk models.Disk) {
 	panic("Unimplemented")
 }
 
-func (d *MockDisk) IsReady() bool {
-	return true
+func (d *MockDisk) GetReadiness() models.DiskReadiness {
+	return nil
 }
 
-func (d *MockDisk) GetResponse(_disk *dbo.Disk) *models.DiskResponse {
+func (d *MockDisk) GetResponse(_disk *dbo.Disk, ctx *gin.Context) *models.DiskResponse {
 	return nil
 }
 
