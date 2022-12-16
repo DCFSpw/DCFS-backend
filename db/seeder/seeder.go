0f4ccf5744d88cbbf887643099ea16a71e58b0fa
Brotholomew <bartoszek.blach@gmail.com>
2022-12-16 17:37:01 +0100

    [DCFS-169][DCFS-176] automatic provider detection and seeding
    Signed-off-by: Brotholomew <bartoszek.blach@gmail.com>

--- db/seeder/seeder.go
+++ db/seeder/seeder.go
@@ -3,11 +3,9 @@ package seeder
 // TODO: Move seeding to another package to avoid cyclic imports
 
 import (
-	"dcfs/constants"
 	"dcfs/db"
 	"dcfs/db/dbo"
 	"dcfs/models"
-	"github.com/google/uuid"
 )
 
 // Seed - add necessary entries to database
@@ -16,11 +14,6 @@ import (
 // doesn't contain these entries. It creates providers for disks and sample
 // user account with empty volume.
 func Seed() {
-	provider1 := dbo.Provider{}
-	provider2 := dbo.Provider{}
-	provider3 := dbo.Provider{}
-	provider4 := dbo.Provider{}
-	providerRAID1 := dbo.Provider{}
 	user := dbo.User{}
 
 	// Add a root user
@@ -34,55 +27,8 @@ func Seed() {
 		db.DB.DatabaseHandle.Create(&user)
 	}
 
-	// Add providers
-	db.DB.DatabaseHandle.Where("type = ?", constants.PROVIDER_TYPE_SFTP).First(&provider1)
-	if provider1.Type != constants.PROVIDER_TYPE_SFTP {
-		provider1.UUID = uuid.New()
-		provider1.Type = constants.PROVIDER_TYPE_SFTP
-		provider1.Name = "SFTP drive"
-		provider1.Logo = "https://cdn.iconscout.com/icon/free/png-256/sftp-1758329-1496548.png"
-
-		db.DB.DatabaseHandle.Create(&provider1)
-	}
-
-	db.DB.DatabaseHandle.Where("type = ?", constants.PROVIDER_TYPE_GDRIVE).First(&provider2)
-	if provider2.Type != constants.PROVIDER_TYPE_GDRIVE {
-		provider2.UUID = uuid.New()
-		provider2.Type = constants.PROVIDER_TYPE_GDRIVE
-		provider2.Name = "GoogleDrive"
-		provider2.Logo = "https://upload.wikimedia.org/wikipedia/commons/d/da/Google_Drive_logo.png"
-
-		db.DB.DatabaseHandle.Create(&provider2)
-	}
-
-	db.DB.DatabaseHandle.Where("type = ?", constants.PROVIDER_TYPE_ONEDRIVE).First(&provider3)
-	if provider3.Type != constants.PROVIDER_TYPE_ONEDRIVE {
-		provider3.UUID = uuid.New()
-		provider3.Type = constants.PROVIDER_TYPE_ONEDRIVE
-		provider3.Name = "OneDrive"
-		provider3.Logo = "https://upload.wikimedia.org/wikipedia/commons/3/3c/Microsoft_Office_OneDrive_%282019%E2%80%93present%29.svg"
-
-		db.DB.DatabaseHandle.Create(&provider3)
-	}
-
-	db.DB.DatabaseHandle.Where("type = ?", constants.PROVIDER_TYPE_FTP).First(&provider4)
-	if provider4.Type != constants.PROVIDER_TYPE_FTP {
-		provider4.UUID = uuid.New()
-		provider4.Type = constants.PROVIDER_TYPE_FTP
-		provider4.Name = "FTP drive"
-		provider4.Logo = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Antu_gFTP.svg/640px-Antu_gFTP.svg.png"
-
-		db.DB.DatabaseHandle.Create(&provider4)
-	}
-
-	// Add a virtual disk provider for representing virtual disks
-	db.DB.DatabaseHandle.Where("type = ?", constants.PROVIDER_TYPE_RAID1).First(&providerRAID1)
-	if providerRAID1.Type != constants.PROVIDER_TYPE_RAID1 {
-		providerRAID1.UUID = uuid.New()
-		providerRAID1.Type = constants.PROVIDER_TYPE_RAID1
-		providerRAID1.Name = "RAID1 virtual drive"
-		providerRAID1.Logo = ""
-
-		db.DB.DatabaseHandle.Create(&providerRAID1)
+	// Add initialized providers
+	for _, providerInit := range models.ProviderTypesRegistry {
+		providerInit()
 	}
 }
