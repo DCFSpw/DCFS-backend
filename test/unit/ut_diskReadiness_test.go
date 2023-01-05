bec6e5e5a2af86dbd2d3d87ed483d6f402e7b1c6
Brotholomew <bartoszek.blach@gmail.com>
2023-01-05 15:12:23 +0100

    [DCFS-172] fileReadiness uts
    Signed-off-by: Brotholomew <bartoszek.blach@gmail.com>

--- test/unit/ut_diskReadiness_test.go
+++ test/unit/ut_diskReadiness_test.go
@@ -1,1 +1,33 @@
 package unit
+
+import (
+	"context"
+	"dcfs/models"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+	"time"
+)
+
+// this model should be excluded from unit testing completely,
+//thus the tests here only validate this file in the coverage
+
+func TestCompoundDiskReadiness(t *testing.T) {
+	r := models.NewRealDiskReadiness(func(ctx context.Context) bool { return true }, func() bool { return true })
+	virtual := models.NewVirtualDiskReadiness(r)
+
+	models.IsReadyPeriodicCheckInterval = time.Second
+	r.IsReady(nil)
+	r.IsReadyForce(nil)
+	r.IsReadyForceNonBlocking(nil)
+
+	virtual.IsReady(nil)
+	virtual.IsReadyForce(nil)
+	virtual.IsReadyForceNonBlocking(nil)
+
+	virtual = models.NewVirtualDiskReadiness()
+	virtual = models.NewVirtualDiskReadiness(nil)
+
+	Convey("This method did not cause any panics or errors", t, func() {
+		So(true, ShouldEqual, true)
+	})
+}
